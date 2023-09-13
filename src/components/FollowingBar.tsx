e1541b4687bbd38f0b86e3b859a678cbdd768212
corner <corner3499@gmail.com>
2023-09-13 15:31:21 +0900

    following style

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,45 +1,33 @@
 'use client';
 
-import useSWR from 'swr';
 import { DetailUser } from '@/model/user';
-import { PropagateLoader } from 'react-spinners';
 import Link from 'next/link';
-import Avatar from '@/components/Avatar';
-import ScrollableBar from '@/components/ui/ScrollableBar';
+import { PropagateLoader } from 'react-spinners';
+import useSWR from 'swr';
+import Avatar from './Avatar';
+import ScrollableBar from './ui/ScrollableBar';
 
 export default function FollowingBar() {
-    const { data, isLoading, error } = useSWR<DetailUser>('/api/me');
+    const { data, isLoading: loading, error } = useSWR<DetailUser>('/api/me');
+    // const users = data?.following;
     // const users = undefined;
-    const users = data?.following && [
-        ...data?.following,
-        ...data?.following,
-        ...data?.following,
-        ...data?.following,
-        ...data?.following
-    ];
-
+    const users = data?.following && [...data?.following, ...data?.following, ...data?.following];
     return (
         <section className="w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
-            {isLoading ? (
+            {loading ? (
                 <PropagateLoader size={8} color="red" />
             ) : (
                 (!users || users.length === 0) && <p>{`You don't have following`}</p>
             )}
             {users && users.length > 0 && (
-                <ul>
-                    <ScrollableBar>
-                        {users.map(({ image, username }, index) => (
-                            <li key={`${username} - ${index}`}>
-                                <Link className="flex flex-col items-center w-20" href={`/user/${username}`}>
-                                    <Avatar image={image} highlight />
-                                    <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
-                                        {username}
-                                    </p>
-                                </Link>
-                            </li>
-                        ))}
-                    </ScrollableBar>
-                </ul>
+                <ScrollableBar>
+                    {users.map(({ image, username }) => (
+                        <Link key={username} className="flex flex-col items-center w-20" href={`/user/${username}`}>
+                            <Avatar image={image} highlight />
+                            <p className="w-full text-sm text-center text-ellipsis overflow-hidden">{username}</p>
+                        </Link>
+                    ))}
+                </ScrollableBar>
             )}
         </section>
     );
