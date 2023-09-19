9b49d895a80858319340073804c7b28301bad216
corner <corner3499@gmail.com>
2023-09-19 18:51:16 +0900

    layout 골격

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className={sans.className}>
-            <body className="w-full bg-neutral-50 overflow-auto">
+            <body className="flex flex-col w-full mx-auto">
                 <AuthContext>
                     <header className="sticky top-0 bg-white z-10 border-b">
                         <div className="max-w-screen-xl mx-auto">
