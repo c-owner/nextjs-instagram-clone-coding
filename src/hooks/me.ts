e3f05f1ca82ea124d164e387453ccf0de600f442
corner <corner3499@gmail.com>
2023-09-20 19:29:29 +0900

    성능 개선 > useCallBack

--- src/hooks/me.ts
+++ src/hooks/me.ts
@@ -1,5 +1,6 @@
 import { HomeUser } from '@/model/user';
 import useSWR from 'swr';
+import { useCallback } from 'react';
 
 async function updateBookmark(postId: string, bookmark: boolean) {
     return fetch('/api/bookmarks', {
@@ -11,21 +12,24 @@ async function updateBookmark(postId: string, bookmark: boolean) {
 export default function useMe() {
     const { data: user, isLoading, error, mutate } = useSWR<HomeUser>('/api/me');
 
-    const setBookmark = (postId: string, bookmark: boolean) => {
-        if (!user) return;
-        const { bookmarks } = user;
-        const newUser = {
-            ...user,
-            bookmarks: bookmark ? [...bookmarks, postId] : bookmarks.filter((b) => b !== postId)
-        };
+    const setBookmark = useCallback(
+        (postId: string, bookmark: boolean) => {
+            if (!user) return;
+            const { bookmarks } = user;
+            const newUser = {
+                ...user,
+                bookmarks: bookmark ? [...bookmarks, postId] : bookmarks.filter((b) => b !== postId)
+            };
 
-        // eslint-disable-next-line consistent-return
-        return mutate(updateBookmark(postId, bookmark), {
-            optimisticData: newUser,
-            populateCache: false,
-            revalidate: false,
-            rollbackOnError: true
-        });
-    };
+            // eslint-disable-next-line consistent-return
+            return mutate(updateBookmark(postId, bookmark), {
+                optimisticData: newUser,
+                populateCache: false,
+                revalidate: false,
+                rollbackOnError: true
+            });
+        },
+        [user, mutate]
+    );
     return { user, isLoading, error, setBookmark };
 }
