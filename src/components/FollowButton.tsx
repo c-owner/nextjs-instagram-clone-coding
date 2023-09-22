77536514d977e00e15e2a37a21eccbfef8ff1b9e
corner <corner3499@gmail.com>
2023-09-22 13:31:24 +0900

    following

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,19 +1,27 @@
 'use client';
 
-import { ProfileUser } from '@/model/user';
-import Button from '@/components/ui/Button';
 import useMe from '@/hooks/me';
+import { ProfileUser } from '@/model/user';
+import Button from './ui/Button';
 
 type Props = {
     user: ProfileUser;
 };
 export default function FollowButton({ user }: Props) {
     const { username } = user;
-    const { user: loggedInUser } = useMe();
+    const { user: loggedInUser, toggleFollow } = useMe();
+
     const showButton = loggedInUser && loggedInUser.username !== username;
-    const isFollowing =
+    const following =
         loggedInUser && loggedInUser.following.find((item) => item.username === username);
 
-    const text = isFollowing ? 'Unfollow' : 'Follow';
-    return <>{showButton && <Button text={text} onClick={() => {}} red={text === 'Unfollow'} />}</>;
+    const text = following ? 'Unfollow' : 'Follow';
+
+    const handleFollow = () => {
+        toggleFollow(user.id, !following);
+    };
+
+    return (
+        <>{showButton && <Button text={text} onClick={handleFollow} red={text === 'Unfollow'} />}</>
+    );
 }
--- src/hooks/me.ts
+++ src/hooks/me.ts
@@ -1,11 +1,18 @@
 import { HomeUser } from '@/model/user';
-import useSWR from 'swr';
 import { useCallback } from 'react';
+import useSWR from 'swr';
 
 async function updateBookmark(postId: string, bookmark: boolean) {
     return fetch('/api/bookmarks', {
         method: 'PUT',
         body: JSON.stringify({ id: postId, bookmark })
+    }).then((res) => res.json());
+}
+
+async function updateFollow(targetId: string, follow: boolean) {
+    return fetch('/api/follow', {
+        method: 'PUT',
+        body: JSON.stringify({ id: targetId, follow })
     }).then((res) => res.json());
 }
 
@@ -21,7 +28,6 @@ export default function useMe() {
                 bookmarks: bookmark ? [...bookmarks, postId] : bookmarks.filter((b) => b !== postId)
             };
 
-            // eslint-disable-next-line consistent-return
             return mutate(updateBookmark(postId, bookmark), {
                 optimisticData: newUser,
                 populateCache: false,
@@ -31,5 +37,12 @@ export default function useMe() {
         },
         [user, mutate]
     );
-    return { user, isLoading, error, setBookmark };
+
+    const toggleFollow = useCallback(
+        (targetId: string, follow: boolean) => {
+            return mutate(updateFollow(targetId, follow), { populateCache: false });
+        },
+        [mutate]
+    );
+    return { user, isLoading, error, setBookmark, toggleFollow };
 }
